ef176e14b2bc8a83811ddfbfc5ddb417a4c47b68
Alex Malykh <alexeyheather@gmail.com>
2021-07-12 04:26:54 +0700

    evert "refactoring"
    This reverts commit f0b57036f26bd4500f74c2b275d46ff908c735d4.

--- app/shortener/services.py
+++ app/shortener/services.py
@@ -1,11 +1,12 @@
+
 from django.contrib import messages
-from django.http import HttpRequest, HttpResponseRedirect, Http404
+from django.http import HttpRequest
 from django.contrib.sessions.models import Session
 from django.core.cache import cache
 
 from loguru import logger
 
-from config.settings import REDIS_TTL
+from config.settings import CELERY_TASK_TIME_LIMIT
 from .forms import URLForm
 from .models import URL
 
@@ -50,7 +51,7 @@ def add_url_form_error_messages_to_message_storage(
 
 def add_url_mapping_to_cache(url_code: str, original_url: str) -> None:
     """Добавляет ключ значение {"url_code": "original_url"} в кэш"""
-    cache.set(url_code, original_url, timeout=REDIS_TTL)
+    cache.set(url_code, original_url, timeout=CELERY_TASK_TIME_LIMIT)
     logger.info((
         f'{original_url} was successfully '
         f'saved in cache with code "{url_code}"'
@@ -72,4 +73,5 @@ def get_original_url_from_cache(url_code: str) -> str:
     
     logger.info(f'Found "{original_url}" with code "{url_code}" in cache')
     return original_url
+
 
--- app/shortener/views.py
+++ app/shortener/views.py
@@ -1,15 +1,17 @@
 from loguru import logger
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.views.decorators.http import require_GET, require_POST
-from django.http import HttpResponseRedirect, Http404
+from django.http.response import Http404
+from django.http import HttpResponseRedirect
 
-from .forms import URLForm
 from .models import URL
+from .forms import URLForm
 from .services import (add_urls_in_user_session, build_shortened_url,
-    add_url_form_error_messages_to_message_storage, add_url_mapping_to_cache, get_original_url_from_cache,
-    get_original_url_from_db)
+    add_url_form_error_messages_to_message_storage, add_url_mapping_to_cache,
+    get_original_url_from_cache, get_original_url_from_db)
 
 
 @require_GET
@@ -59,12 +61,17 @@ def redirect_view(request, code):
     """Редиректит на оригинальный URL из кэша, либо из БД, если в кэше
     не найдено"""
     try:
-        cached_original_url = get_original_url_from_cache(code)
-        return HttpResponseRedirect(cached_original_url)
+        original_url_cached = get_original_url_from_cache(code)
+        logger.info((
+            f'Opening "{original_url_cached}" '
+            f'with code "{code}" from cache'
+        ))
+        return HttpResponseRedirect(original_url_cached)
     except ValueError:
         try:
             original_url = get_original_url_from_db(code)
             add_url_mapping_to_cache(code, original_url)
+            logger.info(f'Opening {original_url} with code "{code}" from db')
             return HttpResponseRedirect(original_url)
         except URL.DoesNotExist:
             logger.info(f'URL with code "{code}" does not exist')
